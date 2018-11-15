6d2bc37a7fdff4d0010311aba7560a53c8c6d36e
hodayamar <hodayamar2000@gmail.com>
2018-11-15 18:21:55 +0200

    Initial Status

--- kalah.py
+++ kalah.py
@@ -1,2 +1,18 @@
 class Kalah(object):
-    pass
+
+    def __init__(self, holes, seeds):
+
+        self.board = {"player_one": [seeds] * holes + [0], "player_two": [seeds] * holes + [0]}
+        self.turn = False
+
+    def status(self):
+
+        return tuple((self.board["player_one"]) + (self.board["player_two"]))
+
+game = Kalah(6,4)
+print()
+print(type(game.status()))
+
+
+
+
--- test_kalah.py
+++ test_kalah.py
@@ -1,8 +1,12 @@
+from kalah import Kalah
 import unittest
 
 
 class KalahTestCase(unittest.TestCase):
-    pass
+
+    def test_status(self):
+
+       assert Kalah(6,4).status() == (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
 
 
 if __name__ == '__main__':
