8d8733513adbd16ab8a2b7b71a3798e93cead547
hodayamar <hodayamar2000@gmail.com>
2018-11-20 17:56:17 +0200

    End Game - Section 1.5 is done!

--- kalah.py
+++ kalah.py
@@ -8,13 +8,12 @@ class Kalah(object):
         self.game_over = False
         self.current_player = 0
 
-
     def status(self):
 
-        return tuple(self.board[0 : self.holes] + [self.bank[0]] +\
-                     self.board[self.holes : (self.holes * 2) + 1] + [self.bank[1]])
+        return tuple(self.board[0: self.holes] + [self.bank[0]] + \
+                     self.board[self.holes: (self.holes * 2) + 1] + [self.bank[1]])
 
-    def set_status(self, l_board):
+    def set_board(self, l_board):
 
         self.board = l_board
 
@@ -22,48 +21,50 @@ class Kalah(object):
 
         self.bank = l_bank
 
-
-    def valid_hole(self,hole):
+    def if_valid_hole(self, hole):
 
         if hole not in range(0, self.holes):
             raise ValueError("you are not allow to do that!")
 
-        if  not self.current_player and self.board[hole] == 0 or \
-                 self.current_player and self.board[hole + self.holes] == 0:
+        if not self.current_player and self.board[hole] == 0 or \
+                self.current_player and self.board[hole + self.holes] == 0:
             raise ValueError("This hole does not have seeds")
 
     def win(self):
 
         if self.bank[0] == self.bank[1]:
             return "Tie"
+
         msg = "Player 2 wins" if self.bank[1] > self.bank[0] else "Player 1 wins"
         return msg
 
-
-    def current_idex(self,i,hole):
+    def current_index(self, i, hole):
         return (self.current_player * self.holes + hole + 1 + i) % (len(self.board))
 
-    def add_seeds(self, bank,left_seeds,index):
+    def add_seeds(self, bank, left_seeds, index):
 
         if left_seeds:
             if bank:
-
                 self.bank[index] += 1
             else:
                 self.board[index] += 1
 
     def play(self, hole):
 
-        self.valid_hole(hole)
+        self.if_valid_hole(hole)
 
-        sum_of_seeds = self.board[hole + self.current_player * self.holes]
-        self.board[hole + self.current_player * self.holes] = 0
+        hole_for_playing = hole + self.current_player * self.holes
+
+        sum_of_seeds = self.board[hole_for_playing]
+        self.board[hole_for_playing] = 0
 
         index = 0
         left_seeds = sum_of_seeds
 
         for i in range(0, sum_of_seeds):
-            index = self.current_idex(i, hole)
+
+            index = self.current_index(i, hole)
+
             if not self.current_player and index == self.holes:
                 self.add_seeds(1, left_seeds, 0)
                 left_seeds -= 1
@@ -76,26 +77,21 @@ class Kalah(object):
             left_seeds -= 1
 
         last_index = index + left_seeds
-        opposite_index = self.holes * 2 -1 - last_index
+        opposite_index = self.holes * 2 - 1 - last_index
+
         if self.board[last_index] == 1 and self.board[opposite_index] != 0:
             robbery = self.board[opposite_index] + self.board[last_index]
             self.board[opposite_index] = 0
             self.board[last_index] = 0
             self.bank[self.current_player] += robbery
 
-
         if index != self.holes and index != 0:
             self.current_player = not self.current_player
 
-
         f_index = (self.current_player) * self.holes
-        s_index = self.holes * ((self.current_player) + 1) -1
+        s_index = self.holes * ((self.current_player) + 1) - 1
 
-        # print(f"first loc: {f_index}")
-        # print(f"second loc: {s_index}")
-
-        if sum(self.board[f_index : s_index +1]) == 0:
-
+        if sum(self.board[f_index: s_index + 1]) == 0:
             self.bank[self.current_player] += sum(self.board)
             self.game_over = True
 
@@ -103,21 +99,12 @@ class Kalah(object):
             win = self.win()
             return win
 
-
-        return  f"Player {self.current_player + 1} plays next"
+        return f"Player {self.current_player + 1} plays next"
 
     def done(self):
 
         return self.game_over
 
     def score(self):
-        return (self.bank[0],self.bank[1])
 
-# game = Kalah(6,4)
-#
-# game.set_status([0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-# game.set_bank([0,0])
-# print(game.status())
-#
-# game.play(2)
-# print(game.status())
+        return (self.bank[0], self.bank[1])
--- test_kalah.py
+++ test_kalah.py
@@ -48,15 +48,15 @@ class KalahTestCase(unittest.TestCase):
 
     def test_captures(self):
 
-        self.game.set_status([0, 0, 4, 4, 4, 9, 5, 5, 5, 4, 4, 4])
+        self.game.set_board([0, 0, 4, 4, 4, 9, 5, 5, 5, 4, 4, 4])
         self.game.play(5)
         assert self.game.status() == (1, 0, 4, 4, 4, 0, 7, 6, 6, 6, 5, 0, 5, 0)
 
-        self.game.set_status([4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 8])
+        self.game.set_board([4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 8])
         self.game.play(5)
         assert self.game.status() == (5, 5, 5, 5, 5, 0, 7, 0, 4, 4, 4, 4, 0, 7)
 
-        self.game.set_status([1, 0, 4, 5, 4, 5, 7, 7, 6, 6, 0, 3])
+        self.game.set_board([1, 0, 4, 5, 4, 5, 7, 7, 6, 6, 0, 3])
         self.game.set_bank([0,0])
         self.game.play(0)
         assert self.game.status() == (0, 1, 4, 5, 4, 5, 0, 7, 7, 6, 6, 0, 3, 0)
@@ -64,19 +64,19 @@ class KalahTestCase(unittest.TestCase):
 
     def test_f_play_win(self):
 
-        self.game.set_status([0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1])
+        self.game.set_board([0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1])
         self.game.set_bank([5,1])
         assert self.game.play(5) == "Player 1 wins"
 
     def test_s_play_win(self):
 
-        self.game.set_status([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+        self.game.set_board([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
         self.game.set_bank([0,0])
         self.game.play(1)
         assert self.game.play(5) == "Player 2 wins"
 
     def test_tie(self):
-        self.game.set_status([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+        self.game.set_board([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
         self.game.set_bank([2, 0])
         self.game.play(1)
         assert self.game.play(5) == "Tie"
